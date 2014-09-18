e4b1716bc8bea157a17c517958ac5e395b859ef5
Adam <amarinelli@esri.ca>
2014-09-18 17:23:08 -0400

    Updated Kimono api
    - using meta API and therefore modified code to select from either of
    the three and input into table

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	console.log('ready');
 	$.ajax({
-		"url":"https://www.kimonolabs.com/api/69auren6?apikey=dHMljbNpnmzYGGsQ7XfqOeIoxw9zGGQI&callback=kimonoCallback",
+		"url":"https://www.kimonolabs.com/api/6q5hyhqo?apikey=dHMljbNpnmzYGGsQ7XfqOeIoxw9zGGQI&callback=kimonoCallback",
 		"crossDomain":true,
 		"dataType":"jsonp",
 		"complete": function(){
@@ -27,21 +27,44 @@ $(document).ready(function() {
 	});		
 });
 
-function kimonoCallback(data) {	
-	var table = document.getElementById("tableBody");
+function kimonoCallback(data) {
 	var results = data.results;
-	var collection1 = results.collection1;
-	for (index=1;index<301;index++) {
+	var collection1 = results.data;
+	/* $.each(results, function() { 
+		alert(this.data.length);
+    }); */	
+		
+	var table = document.getElementById("tableBody");	
+	
+	
+	for (index=1;index<600;index++) {
 		var event = collection1[index];
-		var tr = document.createElement("TR");
-		var newcell1 = tr.insertCell(-1);
-		var newcell2 = tr.insertCell(-1);
-		var newcell3 = tr.insertCell(-1);
-		var newcell4 = tr.insertCell(-1);
-		newcell1.innerHTML=event.artist;		
-		newcell2.innerHTML=event.venue;		
-		newcell3.innerHTML=event.date;		
-		newcell4.innerHTML=event.price;		
-		table.appendChild(tr);
+		if ((/^ARTIST/).test(event.artist) != true) {
+			var tr = document.createElement("TR");
+			var newcell1 = tr.insertCell(-1);
+			var newcell2 = tr.insertCell(-1);
+			var newcell3 = tr.insertCell(-1);
+			var newcell4 = tr.insertCell(-1);
+			
+			if (event.api == "collective-concerts") {
+				newcell1.innerHTML=event.artist.text;		
+				newcell2.innerHTML=event.venue;		
+				newcell3.innerHTML=event.date;		
+				newcell4.innerHTML=event.price;
+			} else if (event.api == "just-shows") {
+				newcell1.innerHTML=event.artist.text;		
+				newcell2.innerHTML=event.venue.text;		
+				newcell3.innerHTML=event.date.text;		
+				newcell4.innerHTML=event.price.text;
+			} else {			
+				newcell1.innerHTML=event.artist;		
+				newcell2.innerHTML=event.venue;		
+				newcell3.innerHTML=event.date;		
+				newcell4.innerHTML=event.price;			
+			}		
+		}
+		if (tr != null){ 
+			table.appendChild(tr);
+		}
 	}				
 }
