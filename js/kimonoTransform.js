284fba9c6ff9e9c5a8012690c7809cf9cf7703d6
Adam <amarinelli@esri.ca>
2015-02-06 17:38:46 -0500

    Major changes throughout
    - smooth scroll and visible jQuery libraries
    - map elements
    - map sizing
    - kimono data access and server modifications

---
+++ js/kimonoTransform.js
@@ -0,0 +1,20 @@
+// https://help.kimonolabs.com/hc/en-us/articles/203744354-Modify-your-API-results-with-a-javascript-function-experimental-
+// occurs on Kimono servers
+
+function transform(data) {
+
+  // Remove the source html table headers
+  data.results.data.splice(0,1);
+  data.count -= 1;
+
+  // add click event to venue item
+  shows = data.results.data;
+  shows.forEach(function(show){
+    venue = show.venue;
+    show.venue = "<div onclick='GoogleMapVenue_click(this.innerHTML)' style='cursor: pointer; color: rgb(62, 142, 232)'>"
+      + venue
+      + "</div>";
+  });
+
+  return data;
+}
